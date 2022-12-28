46c039601ba49770ed7ac0cf5bb0eb804fcba4c6
kushalshrestha <kushalshr@gmail.com>
2022-12-27 23:02:13 -0600

    feat: Scanner lesson added

---
+++ src/quick_java_fpp/fundamental/ScannerTest.java
@@ -0,0 +1,14 @@
+package quick_java_fpp.fundamental;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        System.out.println("You wrote: " + sc.nextLine());
+        System.out.println("Type your age");
+        System.out.println("You age: " + sc.nextInt());
+        sc.close();
+    }
+}
