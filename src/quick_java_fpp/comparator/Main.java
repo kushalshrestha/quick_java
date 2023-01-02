0e9eee38b7adf385cb4228372b9e47dd43d25f4a
kushalshrestha <kushalshr@gmail.com>
2023-01-01 22:26:12 -0600

    feat: Comparator - functional interface & inner class concept implemented

---
+++ src/quick_java_fpp/comparator/Main.java
@@ -0,0 +1,38 @@
+package quick_java_fpp.comparator;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        Person[] personsList = {new Person("Kane", 31),
+                new Person("Andriy", 27)};
+        Arrays.sort(personsList, new NameComparator());
+        System.out.println("1. Sorting in ascending order... (using NameComparator)");
+        System.out.println(Arrays.toString(personsList));
+
+        System.out.println("2. Alternative way - lambda expression : Sorting in ascending order... (using NameComparator)");
+        Comparator<Person> comp = (p1, p2) -> {
+            return p1.getName().compareTo(p2.getName());
+        };
+        Arrays.sort(personsList, comp);
+        System.out.println(Arrays.toString(personsList));
+
+
+        System.out.println("3. Alternative way - Anonymous inner class : Sorting in ascending order... (using NameComparator)");
+        Arrays.sort(personsList, new Comparator<Person>() {
+
+            @Override
+            public int compare(Person p1, Person p2) {
+                return p1.getName().compareTo(p1.getName());
+            }
+        });
+
+        System.out.println(Arrays.toString(personsList));
+
+        System.out.println("4. Alternative way - Anonymous inner class as lambda : Sorting in ascending order... (using NameComparator)");
+        Arrays.sort(personsList, (p1, p2) -> p1.getName().compareTo(p1.getName()));
+
+        System.out.println(Arrays.toString(personsList));
+    }
+}
---
+++ src/quick_java_fpp/comparator/Person.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package quick_java_fpp.comparator;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name='" + name + "', age=" + age + "}";
+    }
+}
