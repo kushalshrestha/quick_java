0d425a0b864d8a703c1155d3012bb19a4d233179
kushalshrestha <kushalshr@gmail.com>
2023-01-01 20:49:34 -0600

    feat: outer this and inner this

--- src/quick_java_fpp/nested_classes/OuterClass.java
+++ src/quick_java_fpp/nested_classes/OuterClass.java
@@ -14,20 +14,24 @@ package quick_java_fpp.nested_classes;
 public class OuterClass {
 
     private int x = 10;
+    private String outerThis = "Outer this!!!";
 
     OuterClass() {
-
         InnerClass innerClass = new InnerClass();
         System.out.println(innerClass.innerVariable);
         innerClass.innerClassMethod();
+        System.out.println(innerClass.innerThis);
+        System.out.println(this.outerThis);
     }
 
     public class InnerClass {
         private int innerVariable = 3;
+        private String innerThis = "Inner this!!!";
 
         public void innerClassMethod() {
             System.out.println("This is inner class method");
             System.out.println("Value of x is : " + x);
+            System.out.println(OuterClass.this.outerThis);
         }
     }
 
