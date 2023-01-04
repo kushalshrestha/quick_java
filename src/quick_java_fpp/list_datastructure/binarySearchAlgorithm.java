c00dc7a253b231b54aab18210a9a327782f75a12
kushalshrestha <kushalshr@gmail.com>
2023-01-03 20:21:12 -0600

    feat: Iterator

--- src/quick_java_fpp/list_datastructure/binarySearchAlgorithm.java
+++ src/quick_java_fpp/list_datastructure/binarySearchAlgorithm.java
@@ -1,5 +1,10 @@
 package quick_java_fpp.list_datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class binarySearchAlgorithm {
     public static int binarySearchUsingIteration(int[] sortedArray, int key, int low, int high) {
         int index = Integer.MAX_VALUE;
@@ -23,5 +28,22 @@ public class binarySearchAlgorithm {
         int key = 4;
         int index = binarySearchUsingIteration(sortedArray, 4, 0, sortedArray.length - 1);
         System.out.println(key + " found at index = " + index);
+
+        List<String> myList = new ArrayList<>();
+        myList.add("Messi");
+        myList.add("Ronaldo");
+        myList.add("Neymar");
+        myList.add("Mbappe");
+        myList.add("Haaland");
+
+        System.out.println("Implementing an Iterator - using direct use");
+        Iterator<String> itr = myList.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        System.out.println("Implementing an Iterator - using for each construct");
+        for(String s : myList) {
+            System.out.println(s);
+        }
     }
 }
