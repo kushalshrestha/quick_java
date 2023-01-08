05515da68876845c2d484adfaf9043d9bf6ffc10
kushalshrestha <kushalshr@gmail.com>
2023-01-08 14:31:08 -0600

    feat: DIP - Dependency Inversion Principle example

---
+++ src/design_patterns/solid_design_principles/DIPDemo.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2023.
+ * Author : Kushal Shrestha
+ * LinkedIn : https://www.linkedin.com/in/kushal-shrestha/
+ * Email : kushalshr@gmail.com
+ */
+
+package design_patterns.solid_design_principles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class DIPDemo {
+    public static void main(String[] args) {
+        Person parent = new Person("John");
+        Person child1 = new Person("Chris");
+        Person child2 = new Person("Matt");
+
+        // low-level module
+        Relationships relationships = new Relationships();
+        relationships.addParentAndChild(parent, child1);
+        relationships.addParentAndChild(parent, child2);
+
+        new Research(relationships);
+    }
+}
+
+// A. High-level modules should not depend on low-level modules.
+// Both should depend on abstractions.
+
+// B. Abstractions should not depend on details.
+// Details should depend on abstractions.
+
+enum Relationship {
+    PARENT,
+    CHILD,
+    SIBLING
+}
+
+class Person {
+    public String name;
+    // dob etc.
+
+
+    public Person(String name) {
+        this.name = name;
+    }
+}
+
+interface RelationshipBrowser {
+    List<Person> findAllChildrenOf(String name);
+}
+
+//low level module - Relationships | Reason: related to data storage and just gives data access in the form of list
+class Relationships implements RelationshipBrowser {
+    private List<Triplet<Person, Relationship, Person>> relations =
+            new ArrayList<>();
+
+    @Override
+    public List<Person> findAllChildrenOf(String name) {
+
+        return relations.stream()
+                .filter(x -> Objects.equals(x.getValue0().name, name)
+                        && x.getValue1() == Relationship.PARENT)
+                .map(Triplet::getValue2)
+                .collect(Collectors.toList());
+    }
+
+    // Triplet class requires javatuples
+
+    public List<Triplet<Person, Relationship, Person>> getRelations() {
+        return relations;
+    }
+
+    public void addParentAndChild(Person parent, Person child) {
+        relations.add(new Triplet<>(parent, Relationship.PARENT, child));
+        relations.add(new Triplet<>(child, Relationship.CHILD, parent));
+    }
+}
+
+// high level module - allows to perform some sort of operations and higher/near to end users and they require only the results.
+// high level module should not depend upon low level module
+class Research {
+
+    // Issue: Here Research depends upon low level module 'Relationships'
+    public Research(Relationships relationships) {
+        // high-level: find all of john's children
+        List<Triplet<Person, Relationship, Person>> relations = relationships.getRelations();
+        relations.stream()
+                .filter(x -> x.getValue0().name.equals("John")
+                        && x.getValue1() == Relationship.PARENT)
+                .forEach(ch -> System.out.println("John has a child called " + ch.getValue2().name));
+    }
+
+    // Solution: Research does not depend upon low level module 'Relationships'. But depends upon RelationshipBrowser i.e depends upon abstractions
+    public Research(RelationshipBrowser browser) {
+        List<Person> children = browser.findAllChildrenOf("John");
+        for (Person child : children)
+            System.out.println("John has a child called " + child.name);
+    }
+}
+
+class Triplet<A, B, C> {
+    A a;
+    B b;
+    C c;
+
+    public Triplet(A a, B b, C c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Triplet() {
+    }
+
+    public A getValue0() {
+        return a;
+    }
+
+    public B getValue1() {
+        return b;
+    }
+
+    public C getValue2() {
+        return c;
+    }
+}
+
+
+
+
+
