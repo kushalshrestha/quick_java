9c483f995e0f2cd0195fc559d35e4477fa1bed54
kushalshrestha <kushalshr@gmail.com>
2023-01-09 21:15:42 -0600

    feat: Factory Design pattern demo

---
+++ src/design_patterns/factory_design_pattern/PointDemo.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023.
+ * Author : Kushal Shrestha
+ * LinkedIn : https://www.linkedin.com/in/kushal-shrestha/
+ * Email : kushalshr@gmail.com
+ */
+
+package design_patterns.factory_design_pattern;
+
+public class PointDemo {
+    public static void main(String[] args) {
+        Point p = Point.Factory.newPolarPoint(2, 3); // reason why static is used in newPolarPoint() and newCartesianPoint() methods
+    }
+}
+
+class Point {
+    private double x, y;
+
+    // constructor not public, explicitly use newCartesianPoint() and newPolarPoint() methods
+    // Imp: make private constructor
+    private Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
+
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+        }
+
+    }
+}
+
+
