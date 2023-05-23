512765270f2bcae2ade89be90b8c4bdab5a46885
kushalshrestha <kushalshr@gmail.com>
2023-05-22 23:56:04 -0500

    feat: innerclass example

---
+++ src/quick_java_fpp/fundamental/thisKeyword.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023.
+ * Author : Kushal Shrestha
+ * Linkedin : https://www.linkedin.com/in/kushal-shrestha/
+ * Email : kushalshr@gmail.com
+ */
+
+package quick_java_fpp.fundamental;
+
+/*
+Tags: thisKeyword
+* In Java, this keyword is a reference to the current object whose method is being called.
+ */
+public class thisKeyword {
+
+}
