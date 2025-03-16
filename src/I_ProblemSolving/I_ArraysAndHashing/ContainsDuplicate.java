d8fcb6ab1ff9fa485ef8576f8751f914d47d9ff2
kushalshrestha <kushalshr@gmail.com>
2025-03-16 13:10:54 -0700

    feat: Problem Solving - Contains Duplicate

---
+++ src/I_ProblemSolving/I_ArraysAndHashing/ContainsDuplicate.java
@@ -0,0 +1,70 @@
+package I_ProblemSolving.I_ArraysAndHashing;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*Contains Duplicate
+Solved 
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+*/
+
+public class ContainsDuplicate {
+	
+	/* 1. By Brute Force */
+	public boolean hasDuplicate(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] == nums[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	 /* 2. By Sorting */
+	public boolean hasDuplicateBySorting(int[] nums) {
+		Arrays.sort(nums);
+		for (int i=1; i < nums.length; i++) {
+			if (nums[i] == nums[i-1]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean hasDuplicateByHashSet(int[] nums) {
+		Set<Integer> seen = new HashSet<Integer>();
+		for (int num: nums) {
+			if (seen.contains(num)) {
+				return true;
+			}
+			seen.add(num);
+		}
+		
+		return false;
+	}
+	
+	// Test method
+    public static void main(String[] args) {
+    	ContainsDuplicate solution = new ContainsDuplicate();
+    	int[] nums = {1, 2, 3, 3};
+    	System.out.println(solution.hasDuplicate(nums)); //true
+    	System.out.println(solution.hasDuplicateBySorting(nums));
+    	System.out.println(solution.hasDuplicateByHashSet(nums));
+        
+    }
+}
