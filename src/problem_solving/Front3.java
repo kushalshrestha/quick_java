9e78b3b4ef99d541f150f3355c2453bc66469cc1
kushalshrestha <kushalshr@gmail.com>
2026-01-23 15:08:19 -0800

    feat: Front3 problem

--- src/problem_solving/Front3.java
+++ src/problem_solving/Front3.java
@@ -6,6 +6,7 @@
  */
 
 package problem_solving;
+
 /*
 * Given a string, we'll say that the front is the first 3 chars of the string.
 * If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
@@ -16,5 +17,22 @@ front3("Chocolate") → "ChoChoCho"
 front3("abc") → "abcabcabc"
 * */
 public class Front3 {
+    public String front3(String text) {
+        String finalText = "";
+        if (text.length() < 3) {
+            finalText = text;
+        } else {
+            finalText = text.substring(0, 3);
+        }
+        return finalText + finalText + finalText;
+    }
 
+    public static void main(String[] args) {
+        Front3 f3 = new Front3();
+
+        System.out.println(f3.front3("Java"));
+        System.out.println(f3.front3("Chocolate"));
+        System.out.println(f3.front3("abc"));
+        System.out.println(f3.front3("a"));
+    }
 }
